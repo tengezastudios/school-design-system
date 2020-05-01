4f401b13d5a55e99f990a980ad0247087316adc6
gachez <briangach98@gmail.com>
2020-05-01 10:44:07 +0300

    add base button component

--- src/Atoms/Buttons/Button.js
+++ src/Atoms/Buttons/Button.js
@@ -1,14 +1,19 @@
-import react from 'react';
+import React from 'react';
 import '../../styles/style.scss';
 
-buttonStructure= {
-    width: '6.5rem',
-    height: '2.5rem'
-}
  export default  () => {
       return(
           <>
-          <Button style={buttonStructure}>BUTTON</Button>
+         <div className="interactive_focus">
+          
+          <div className="button button_base_theme" >
+              
+              <p className="button_text">BUTTON</p>
+              
+              </div>   
+          
+          </div> 
+          
           </>
       )  
     
---
+++ src/PreviewButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './Atoms/Buttons/Button';
+
+export default class PreviewButton extends React.Component {
+    render() {
+        return(
+            <div style={{width: '100%', height: '100%'}}>
+                <Button />
+            </div>
+        )
+    }
+}
