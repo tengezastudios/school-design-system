aa77ae7dfffa32c9c67431ef4a1d1534a4aeee47
gachez <briangach98@gmail.com>
2020-05-01 09:20:03 +0300

    changes

---
+++ src/Atoms/Buttons/Button.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import '../../styles/style.scss';
+
+buttonStructure= {
+    width: '6.5rem',
+    height: '2.5rem'
+}
+ export default  () => {
+      return(
+          <>
+          <Button style={buttonStructure}>BUTTON</Button>
+          </>
+      )  
+    
+}
