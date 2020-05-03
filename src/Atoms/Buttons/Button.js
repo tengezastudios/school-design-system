522b87606b230f4d2e6614ffad56ee5673d90841
gachez <briangach98@gmail.com>
2020-05-03 15:41:59 +0300

    deploy storybook

--- src/Atoms/Buttons/Button.js
+++ src/Atoms/Buttons/Button.js
@@ -5,7 +5,7 @@ export const Button = ({cls, children, border, fn, icon, clsIcon, iconStroke}) =
     <div className="interactive_focus" style={{border: border}}>
         <div className={cls} onClick={() => fn()}>
         <svg style={{display: icon}} className={clsIcon} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M15 18L9 12L15 6" stroke={iconStroke} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M15 18L9 12L15 6" stroke={iconStroke} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
         {children}
         </div>
--- src/PreviewButton.js
+++ src/PreviewButton.js
@@ -5,7 +5,9 @@ export default class PreviewButton extends React.Component {
     render() {
         return(
             <div style={{width: '100%', height: '100%'}}>
-                <Button />
+            <Button cls="button_icon secondary_button_hover_state" clsIcon="left_icon" border="none" iconStroke="white" fn={console.log('working')}>
+                <p className="button_icon_text">BUTTON</p>
+            </Button>
             </div>
         )
     }
