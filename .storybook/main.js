f52dbdd919627b18966bf9436cd7690a8156a3fa
gachez <briangach98@gmail.com>
2020-05-03 16:04:21 +0300

    update config

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,16 +5,7 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs'
-  ],
-  test: /\.(png|jp(e*)g|svg|gif)$/,
-    use: [
-      {
-        loader: 'file-loader',
-        options: {
-          name: 'images/[hash]-[name].[ext]',
-        },
-      },
-    ],
+  ]
   }
 
 
