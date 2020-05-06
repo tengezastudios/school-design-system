3da9a92c4d40da5470bcf5dffad4c79b4ea499de
gachez <briangach98@gmail.com>
2020-05-06 10:44:45 +0300

    fix: alignments and spacing

--- src/PreviewButton.js
+++ src/PreviewButton.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Button} from './Atoms/Buttons/Button';
 
+
+const $blue50  = '#2662d9';
 export default class PreviewButton extends React.Component {
     render() {
         return(
             <div style={{width: '100%', height: '100%'}}>
-            <Button cls="button_icon secondary_button_hover_state" clsIcon="left_icon" border="none" iconStroke="white" fn={console.log('working')}>
-                <p className="button_icon_text">BUTTON</p>
-            </Button>
+                <Button cls="button_icon tertiary_button_hover_state" clsIcon="right_icon" iconStroke={$blue50} border="none" fn={console.log('primary focus button')}>
+                    <p className="button_tertiary_text_right">BUTTON</p>
+                </Button>
             </div>
         )
     }
