d6234b9e8dbe59615d21d181817bc783ad947378
gachez <briangach98@gmail.com>
2020-05-12 10:07:00 +0300

    added the tag component along with its states

---
+++ src/Atoms/Tags/Tag.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../../styles/style.scss';
+
+export const Tag = ({children, cls}) => (
+    <div className={cls}>
+        {children}
+        </div>
+    
+)
---
+++ src/Atoms/Tags/Tag.stories.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Tag} from '../Tags/Tag';
+import '../../styles/style.scss';
+
+export default {
+  title: 'Tags',
+  component: Tag
+};
+
+
+// tag base
+export const TagBase = () => (<Tag cls="tag-container tag-text">Tag label</Tag>);
+
+//tag interactive
+export const TagInteractive = () => (<Tag cls="tag-container-interactive tag-text">
+  Tag label
+    <svg className="interactive-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 4L4 12" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4 4L12 12" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </Tag>);
+
+//tag base group 
+export const TagBaseGroup = () => (<div className="base-group">
+<Tag cls="tag-container tag-text">Tag label</Tag>
+<Tag cls="tag-container tag-text">Tag label</Tag>
+<Tag cls="tag-container tag-text">Tag label</Tag>
+</div>)
+
+//tag interactive group
+export const TagInteractiveGroup = () => (<div className="base-group">
+<Tag cls="tag-container-interactive tag-text">
+  Tag label
+    <svg className="interactive-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 4L4 12" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4 4L12 12" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </Tag>
+  <Tag cls="tag-container-interactive tag-text">
+  Tag label
+    <svg className="interactive-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 4L4 12" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4 4L12 12" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </Tag>
+  <Tag cls="tag-container-interactive tag-text">
+  Tag label
+    <svg className="interactive-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 4L4 12" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4 4L12 12" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </Tag>
+</div>)
