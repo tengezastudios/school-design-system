f90861c8847c0ed9485c245f74380edf29e11609
gachez <briangach98@gmail.com>
2020-05-16 20:23:15 +0300

    added base tag component

--- src/Atoms/Tags/Tag.stories.js
+++ src/Atoms/Tags/Tag.stories.js
@@ -4,10 +4,18 @@ import '../../styles/style.scss';
 
 export default {
   title: 'Tags',
-  component: Tag
 };
 
 //tag base
+export const TagBase = () => (<Tag cls="tag-base-style tag-container-interactive tag-text">
+  Tag label
+    <svg className="interactive-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 4L4 12" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4 4L12 12" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </Tag>);
+
+
 // tag single
 export const TagSingle = () => (<Tag cls="tag-container tag-text">Tag label</Tag>);
 
