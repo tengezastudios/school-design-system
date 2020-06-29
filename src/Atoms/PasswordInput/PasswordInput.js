574faf288d948fd933cd5653ef845d6489b1fc54
gachez <briangach98@gmail.com>
2020-06-29 16:17:38 +0300

    added the password input component and its stories

---
+++ src/Atoms/PasswordInput/PasswordInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../../styles/style.scss';
+
+export const PasswordInput = ({cls, children}) => (
+    <div className={cls}>
+        {children}
+        </div>   
+);
+
