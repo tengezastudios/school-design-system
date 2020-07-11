ae45939ef0ab0b7a4173d420dc6a875999c2e837
gachez <briangach98@gmail.com>
2020-07-11 21:08:24 +0300

    added text input and its storie

---
+++ src/Atoms/TextInput/TextInput.stories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../../styles/style.scss';
+import { TextInput } from './TextInput';
+
+export default {
+  title: 'TextInput',
+  component: TextInput
+};
+
+// Text Input Base
+export const TextInputBase = () => (
+        <TextInput cls="text-input-container">  
+            <p className="text-label-container">Input label</p>
+            <input className="text-input-field" type="text" placeholder="Placeholder Input"/>   
+        </TextInput>
+);
+
+// Text Input Disabled
+export const TextInputDisabled = () => (
+    <TextInput cls="text-input-container">  
+        <p className="text-label-container disabled">Input label</p>
+        <input className="text-input-field disabled-placeholder" type="text" placeholder="Placeholder Input"/>   
+    </TextInput>
+);
+
+// Text Input Error
+export const TextInputError = () => (
+    <TextInput cls="text-input-container">  
+    <p className="text-label-container disabled">Input label</p>
+    <input className="text-input-field disabled-placeholder error" type="text" placeholder="Placeholder Input"/>  
+          <p className="error-text">Wrong password</p>   
+    </TextInput>
+  );
