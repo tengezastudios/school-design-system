e4f7112fa298424d09a86ae5e35223f279989a19
gachez <briangach98@gmail.com>
2021-01-19 20:10:06 +0300

    dropdown base component added

---
+++ src/Atoms/Dropdown/Dropdown.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../styles/style.scss';
+
+export const Dropdown = ({cls, children}) => (
+    <div className={cls}>
+        {children}
+        </div>   
+);
---
+++ src/Atoms/Dropdown/Dropdown.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../styles/style.scss';
+import { Dropdown } from './Dropdown';
+
+export default {
+  title: 'Dropdown',
+  component: Dropdown
+};
+
+// Dropdown Base
+export const DropdownBase = () => (
+        <Dropdown cls="dropdown-base">  
+            <p className="dropdown-label">Dropdown label</p>
+            <div className="dropdown-picker">
+                <span className="choice">Placeholder 1</span>
+                <svg className="icon-down" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4 6L8 10L12 6" stroke="#1A1A1A" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+            </div>
+            <div className="dropdown-list">
+            </div>
+        </Dropdown>
+);
